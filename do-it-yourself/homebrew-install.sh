4f66824972ba3a9060a7dac47e364134ffbf56c3
Kipras Melnikovas <kipras@kipras.org>
2022-12-20 02:53:26 +0200

    brew: add gcc/g++ install instructions (instead of clang)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- do-it-yourself/homebrew-install.sh
+++ do-it-yourself/homebrew-install.sh
@@ -2,3 +2,24 @@
 
 brew bundle install
 
+echo "\
+
+
+custom: to use real gcc/g++ instead of clang,
+create a symlink in your prefix dir (/usr/local/bin on intel mac,
+and /opt/homebrew/bin) for executables, like so:
+
+assuming latest gcc/g++ version 12:
+
+intel mac:
+
+ln -s /usr/local/bin/gcc-12 /usr/local/bin/gcc
+ln -s /usr/local/bin/g++-12 /usr/local/bin/g++
+
+M1 mac:
+
+ln -s /opt/homebrew/bin/gcc-12 /usr/local/bin/gcc
+ln -s /opt/homebrew/bin/g++-12 /usr/local/bin/g++
+
+"
+
